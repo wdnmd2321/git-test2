8463a74551dc31e416e2727b2fa5f56a09f40234
ls <1092257351@qq.com>
2022-06-14 11:53:11 +0800

    first commit

---
+++ src/main/java/com/atls/git/Test01.java
@@ -0,0 +1,15 @@
+package com.atls.git;
+
+public class Test01 {
+    public static void main(String[] args) {
+        int sum = 0;
+        for(int i = 1; i <= 100; i++){
+            for(int j = 1;j <=100;j++){
+                sum +=j;
+            }
+        }
+        System.out.println("sum=" +sum);
+    }
+}
+
+
