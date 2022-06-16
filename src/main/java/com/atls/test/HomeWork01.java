226fc7c08f6b46ef5239e83105a6c4cb2115f60a
ls <1092257351@qq.com>
2022-06-16 11:05:18 +0800

    push commit4

---
+++ src/main/java/com/atls/test/HomeWork01.java
@@ -0,0 +1,15 @@
+package com.atls.test;
+
+public class HomeWork01 {
+    public static void main(String[] args) {
+        String book1 = "天龙八部";
+        String book2 = "笑傲江湖";
+        System.out.println(book1 + book2);
+        char c1 = '男';
+        char c2 = '女';
+        System.out.println(c1 +c2);
+        double price1 = 134.22;
+        double price2 = 232.33;
+        System.out.println(price1 + price2);
+    }
+}
---
+++ src/main/java/com/atls/test/HomeWork02.java
@@ -0,0 +1,14 @@
+package com.atls.test;
+
+public class HomeWork02 {
+    public static void main(String[] args) {
+        String name = "jack";
+        int age = 20;
+        double score = 90.2;
+        char gender = '男';
+        String hobby = "打篮球";
+        System.out.println("姓名\t年龄\t成绩\t性别\t爱好\n" +
+                name + "\t" + age + "\t" + score + "\t" +
+                gender + "\t" + hobby + "\t");
+    }
+}
