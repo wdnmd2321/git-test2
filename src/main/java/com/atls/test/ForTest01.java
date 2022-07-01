d33a38ee3e1f4d5e92b86e7307d7fb342d1eebd1
ls <1092257351@qq.com>
2022-07-01 16:45:38 +0800

    push commit9

---
+++ src/main/java/com/atls/test/ForTest01.java
@@ -0,0 +1,9 @@
+package com.atls.test;
+
+public class ForTest01 {
+    public static void main(String[] args) {
+        for (int i = 0;i<=36;i++){
+            System.out.println("你好java" +i);
+        }
+    }
+}
